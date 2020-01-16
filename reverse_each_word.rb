e190784e1b7f3dad48795ba05045d49d07e6d8d7
Paul Kim <pkim1295@gmail.com>
2020-01-16 20:32:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,13 @@
 def reverse_each_word(string)
   array = string.split(" ")
+  phrase = ""
+  
   array.each do |word|
     count = 0
     while count < array.length do
-      
+      phrase += "#{word.reverse}\n"
     end
   end
   #array.collect {|x| x.reverse}
+  phrase
 end
