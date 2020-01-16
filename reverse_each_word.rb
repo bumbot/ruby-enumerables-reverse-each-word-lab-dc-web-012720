af5827143ca233a31c2e8f37e211b14022620c85
Paul Kim <pkim1295@gmail.com>
2020-01-16 20:55:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,14 +2,14 @@ def reverse_each_word(string)
   array = string.split(" ")
   phrase = ""
   
-  array.each_with_index do |word, index|
-  #array.collect do|x| 
-    #phrase += x.reverse
-    if array[index] == array[-1]
-      phrase += word.reverse
-    else
-      phrase += "#{word.reverse} "
-    end
+  #array.each_with_index do |word, index|
+  array.collect do|x| 
+    phrase += x.reverse
+    #if array[index] == array[-1]
+    #  phrase += word.reverse
+    #else
+    #  phrase += "#{word.reverse} "
+    #end
   end
   
   phrase
