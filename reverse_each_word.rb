660f356f3d0c33b132e523722d0fb4c24104f1cb
Paul Kim <pkim1295@gmail.com>
2020-01-16 20:51:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   
   array.each_with_index do |word, index|
   #array.collect do|x| 
-    
+    #phrase += x.reverse
     if array[index] == array[-1]
       phrase += word.reverse
     else
